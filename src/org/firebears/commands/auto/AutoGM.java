52f42f1b628e69e2619a9f3e7f5c18f6882f051d
jeron <snowtop105@gmail.com>
2015-02-13 19:36:17 -0600

    removed empty implementations of methods in autonomous commands.

--- src/org/firebears/commands/auto/AutoGM.java
+++ src/org/firebears/commands/auto/AutoGM.java
@@ -16,30 +16,8 @@ public class  AutoGM extends CommandGroup {
 
     public AutoGM() {
     	addSequential(new GrabberCommand(true));
-    	//ToDo: add in a way to stop movement depending on where the robot is
-    	addSequential(new ForwardCommand(-1));
+    	//todo: add in a way to stop movement depending on where the robot is
+    	addSequential(new ForwardCommand(-1), 2.0);
     }
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    }
-
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    }
-
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
-
-    // Called once after isFinished returns true
-    protected void end() {
-      	setTimeout(2);
-    }
-
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
 }
--- src/org/firebears/commands/auto/AutoM.java
+++ src/org/firebears/commands/auto/AutoM.java
@@ -11,29 +11,7 @@ public class  AutoM extends CommandGroup {
 
     public AutoM() {
     	//ToDo: add in a way to stop movement depending on where the robot is
-    	addSequential(new ForwardCommand(-1));
+    	addSequential(new ForwardCommand(-1), 1.0);
     }
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    }
-
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    }
-
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
-
-    // Called once after isFinished returns true
-    protected void end() {
-      	setTimeout(1);
-    }
-
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
 }
