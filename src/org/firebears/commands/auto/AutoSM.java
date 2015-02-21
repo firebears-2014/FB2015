41099fab7f4a84c490bbecfd707058389d9c9963
javad <tubaflub@gmail.com>
2015-02-21 16:28:03 -0600

    stuff

--- src/org/firebears/commands/auto/AutoSM.java
+++ src/org/firebears/commands/auto/AutoSM.java
@@ -7,6 +7,7 @@ import org.firebears.commands.grabber.GrabberCommand;
 import org.firebears.commands.lift.SetHeightCommand;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
 
 /**
  * Autonomous Command: Stacks up the 3 totes in the auto zone
@@ -20,18 +21,22 @@ public class AutoSM extends CommandGroup {
 		addSequential(new GrabberCommand(false));
 		addSequential(new GrabberCommand(true));
 		addParallel(new SetHeightCommand("Lift_Tote_2"));
+		addSequential(new WaitCommand(.5));
 		addParallel(new StrafeCommand(1), 1.0); // change interrupt?
 		addParallel(new WidetoteCommand());
 		// tote 2 logic
 		addSequential(new SetHeightCommand("Lift_Tote_1"));
+		addSequential(new WaitCommand(.5));
 		addParallel(new GrabberCommand(false));
 		addParallel(new SetHeightCommand("Lift_Tote_0"));
 		addSequential(new GrabberCommand(true));
 		addParallel(new SetHeightCommand("Lift_Tote_2"));
+		addSequential(new WaitCommand(.5));
 		addParallel(new StrafeCommand(1), 1.0); // change interrupt
 		addParallel(new WidetoteCommand());
 		// tote 3 logic
 		addSequential(new SetHeightCommand("Lift_Tote_1"));
+		addSequential(new WaitCommand(.5));
 		addParallel(new GrabberCommand(false));
 		addParallel(new SetHeightCommand("Lift_Tote_0"));
 		addSequential(new GrabberCommand(true));
@@ -39,8 +44,9 @@ public class AutoSM extends CommandGroup {
 		// scoring logic
 		addSequential(new ForwardCommand(-1, false), 1.0); // change interrupt
 		addSequential(new SetHeightCommand("Lift_Tote_0"));
+		addSequential(new WaitCommand(.5));
 		addSequential(new GrabberCommand(false));
-		addSequential(new ForwardCommand(-.1, false), 1.0); // change interrupt
+		//addSequential(new ForwardCommand(-.1, false), 1.0); // change interrupt
 	}
 
 }
--- src/org/firebears/commands/auto/AutoTM.java
+++ src/org/firebears/commands/auto/AutoTM.java
@@ -1,5 +1,6 @@
 package org.firebears.commands.auto;
 
+import org.firebears.Robot;
 import org.firebears.commands.drive.ForwardCommand;
 import org.firebears.commands.drive.RotateCommand;
 import org.firebears.commands.drive.StrafeCommand;
@@ -9,6 +10,7 @@ import org.firebears.commands.grabber.GrabberCommand;
 import org.firebears.commands.lift.SetHeightCommand;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
 
 
 
@@ -22,22 +24,27 @@ public class  AutoTM extends CommandGroup {
     	//add rotate command
        	//addSequential(new RotateCommand(.75, 90)); //TODO: is right?
     	
-    	addSequential(new SetHeightCommand("Lift_Tote_3"));  //moves container up
-    	addSequential(new StrafeCommand(-1), 1.0);
+    	addSequential(new SetHeightCommand(Robot.lift.LIFT_3_HEIGHT));		//moves container up
+    	addSequential(new WaitCommand(1.5));
+    	addSequential(new StrafeCommand(-1), .6);
 		addSequential(new WidetoteCommand());  					 //centers on tote
-		addSequential(new SetHeightCommand("Lift_Tote_2"));  //moves container down
-		addParallel(new GrabberCommand(false));				//releases
+		addSequential(new SetHeightCommand(Robot.lift.LIFT_2_HEIGHT));
+		addSequential(new WaitCommand(.3));//moves container down
+		addSequential(new GrabberCommand(false));				//releases
 		addSequential(new WidetoteCommand());  
-		addSequential(new SetHeightCommand("Lift_Tote_0"));  //sets arm to floor position
-		addSequential(new WidetoteCommand());  					 //centers again to be sure
-		addSequential(new GrabberCommand(true));  			 //grabs tote
-		addSequential(new SetHeightCommand("Lift_Tote_1"));  //moves conatainer
+		addSequential(new GrabberCommand(false));
+		addSequential(new SetHeightCommand(Robot.lift.LIFT_0_HEIGHT));  //sets arm to floor position 
+		addSequential(new GrabberCommand(true));
+		addSequential(new WaitCommand(.4));//grabs tote
+		addSequential(new SetHeightCommand(Robot.lift.LIFT_1_HEIGHT));  //moves conatainer
     	//todo: add in a way to stop movement depending on where the robot is
-		//rotate -90 degrees 
-    	addSequential(new ForwardCommand(-.5, true), 1.8); //TODO: is right?
+		addSequential(new WaitCommand(.4));
+    	addSequential(new ForwardCommand(-.3, true), 2.0); //TODO: is right?
     	addSequential(new RotateCommand(.75, 90)); //TODO: is right?
-    	addSequential(new SetHeightCommand("Lift_Tote_0"));
+    	addSequential(new SetHeightCommand(Robot.lift.LIFT_0_HEIGHT));
+    	addSequential(new WaitCommand(.5));
     	addSequential(new GrabberCommand(false));
+    	System.out.println("sup");
     }
 
-}
+}	
